e5a27f0b73bab616d4984858df447ec1c5af9b38
José Fernando Chan Dzib <jferchan23@gmail.com>
2025-01-03 16:13:53 -0500

    proyecto inicializado

---
+++ multiplos/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.multiplos',
+  appName: 'multiplos',
+  webDir: 'www'
+};
+
+export default config;
