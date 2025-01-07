636112d51ef5bca8b1dd5901a199a51b49c76dd3
José Fernando Chan Dzib <jferchan23@gmail.com>
2025-01-06 21:40:12 -0500

    Se eliminaron comentarios redundantes

--- multiplos/src/main.ts
+++ multiplos/src/main.ts
@@ -1,10 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { AngularFireModule } from '@angular/fire/compat'; // Asegúrate de importar este módulo
-import { importProvidersFrom } from '@angular/core'; // Importa importProvidersFrom para usar en proveedores
-import { environment } from './environments/environment'; // Asegúrate de tener la configuración de Firebase
-
+import { AngularFireModule } from '@angular/fire/compat'; 
+import { importProvidersFrom } from '@angular/core'; 
+import { environment } from './environments/environment'; 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
@@ -13,7 +12,6 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    // Usamos importProvidersFrom para incluir AngularFireModule
     importProvidersFrom(AngularFireModule.initializeApp(environment.firebaseConfig)),
   ],
 });
